a4659545ee4f0926daed5a681b5a0317942c575e
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2021-06-22 21:32:05 -0400

    Removed more commented code

--- src/main.ino
+++ src/main.ino
@@ -3,49 +3,7 @@
 
 SYSTEM_THREAD(ENABLED);
 
-// void outputThread();
-
-// int outputPin = D6;
-// int inputPin = D7;
-
-// bool BIT1 = true;
-// bool BIT0 = false;
-
-// enum InputState {
-//     initial,
-//     output0,
-//     waitShort,
-//     output1,
-//     waitLong,
-// } CurrentInputState;
-
-
-// enum StateDuration {
-//     shortPeriod, 
-//     longPeriod, 
-//     veryLongPeriod
-// };
-
-// MessageManager msgManager;
-
 bool isVerbose = true;
-
-// // Transmission speed variables
-// int transmissionSpeed = 0;
-// int speedInterrupts = 0;
-// unsigned long firstSpeedInterruptTime = 0;
-
-// bool skippedLastInputEvent = false;
-
-// bool inputCurrentStateHigh = false;
-// int lastChangeTime = 0;
-// int inputClockPeriod = 500;
-// int outputClockPeriod = 500;
-
-// system_tick_t lastThreadTime = 0;
-// system_tick_t lastMessageTime = 0;
-
-// int bitCounter = 0;
 
 void setup() {
 	Serial.begin(9600);
@@ -54,7 +12,6 @@ void setup() {
 
     waitFor(Serial.isConnected, 30000);
     Serial.println("Serial connected: starting");
-    //Thread thread("outputThread", outputThread);
 }
 
 void loop() {
@@ -72,4 +29,3 @@ void loop() {
     // ex. msgManager.sendMessage(message1);
     // delay(10000); OR os_thread_delay_until(&lastMessageTime, 10000)
 }
-
