56991b3f061f62990921143b2b702b2de9f6df0f
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2021-06-22 21:23:56 -0400

    Removed classes, going all in baby

--- src/MessageManager.ino
+++ src/MessageManager.ino
@@ -1,59 +1,50 @@
+#pragma once
+#include "main.ino"
 #include "FrameManager.ino"
 
-class MessageManager {
 
-public:
-    FrameManager frameManager = FrameManager();
+uint8_t* lastMessageSent;
 
-private:
+void sendMessage(uint8_t* messageToSend, uint8_t messageSize) {
+    lastMessageSent = messageToSend;
+    sendDataFrame(messageToSend, messageSize);
+};
 
-    uint8_t* lastMessageSent;
-    bool isVerbose = true;
-
-public:
-
-    void sendMessage(uint8_t* messageToSend) {
-        lastMessageSent = messageToSend;
-        frameManager.sendData(messageToSend);
-    };
-
-    void receiveMessage(uint8_t* messageReceived) {
-        // Have to find way to poll / get message at transmission end
-        uint8_t* receivedMessage = frameManager.receivingFrame.message;
-        compareReadMessage(true, receivedMessage, lastMessageSent, frameManager.receivingFrame.messageLength);
-    };
+void receiveMessage(uint8_t* messageReceived) {
+    // Have to find way to poll / get message at transmission end
+    uint8_t* receivedMessage = receivingFrame.message;
+    compareReadMessage(true, receivedMessage, lastMessageSent, receivingFrame.messageLength);
+};
 
 
-    bool compareReadMessage(bool isString, uint8_t *bytesRead, uint8_t *byteCompare, uint8_t length){
+bool compareReadMessage(bool isString, uint8_t *bytesRead, uint8_t *byteCompare, uint8_t length){
 
-        bool isSame;
-        if (isString){
-            char* msgRead = (char*)bytesRead;
-            char* msgCompare = (char*)byteCompare;
+    bool isSame;
+    if (isString){
+        char* msgRead = (char*)bytesRead;
+        char* msgCompare = (char*)byteCompare;
 
-            isSame = !strcmp(msgRead, msgCompare);
-            // if (isSame)
-            //     Serial.printlnf("SUCCES: Received message: \t %s.", msgRead);
-            // else
-            //     Serial.printlnf("ERROR: Received message: \t Expected %s, Received %s.", msgCompare, msgRead);
+        isSame = !strcmp(msgRead, msgCompare);
+        // if (isSame)
+        //     Serial.printlnf("SUCCES: Received message: \t %s.", msgRead);
+        // else
+        //     Serial.printlnf("ERROR: Received message: \t Expected %s, Received %s.", msgCompare, msgRead);
+    }
+    else{
+        unsigned long receivedSum = 0;
+        unsigned long compareSum = 0;
+        for (int i=0; i < length; i++){
+            receivedSum += bytesRead[i];
+            compareSum += byteCompare[i];
         }
-        else{
-            unsigned long receivedSum = 0;
-            unsigned long compareSum = 0;
-            for (int i=0; i < length; i++){
-                receivedSum += bytesRead[i];
-                compareSum += byteCompare[i];
-            }
 
-            isSame = receivedSum == compareSum;
-            // if (isSame)
-            //     Serial.printlnf("SUCCES: Received message: \t Expected %d, Received %d.", compareSum, receivedSum);
-            // else
-            //     Serial.printlnf("ERROR: Received message: \t Expected %d, Received %d.", compareSum, receivedSum);
-        }
-        
-        return isSame;
-
-    };
+        isSame = receivedSum == compareSum;
+        // if (isSame)
+        //     Serial.printlnf("SUCCES: Received message: \t Expected %d, Received %d.", compareSum, receivedSum);
+        // else
+        //     Serial.printlnf("ERROR: Received message: \t Expected %d, Received %d.", compareSum, receivedSum);
+    }
+    
+    return isSame;
 
 };
--- src/main.ino
+++ src/main.ino
@@ -1,255 +1,75 @@
+#pragma once
 #include "MessageManager.ino"
 
 SYSTEM_THREAD(ENABLED);
 
-void outputThread();
+// void outputThread();
 
-int outputPin = D6;
-int inputPin = D7;
+// int outputPin = D6;
+// int inputPin = D7;
 
-bool BIT1 = true;
-bool BIT0 = false;
+// bool BIT1 = true;
+// bool BIT0 = false;
 
-enum InputState {
-    initial,
-    output0,
-    waitShort,
-    output1,
-    waitLong,
-} CurrentInputState;
+// enum InputState {
+//     initial,
+//     output0,
+//     waitShort,
+//     output1,
+//     waitLong,
+// } CurrentInputState;
 
 
-enum StateDuration {
-    shortPeriod, 
-    longPeriod, 
-    veryLongPeriod
-};
+// enum StateDuration {
+//     shortPeriod, 
+//     longPeriod, 
+//     veryLongPeriod
+// };
 
-MessageManager msgManager;
+// MessageManager msgManager;
 
-// Transmission speed variables
-int transmissionSpeed = 0;
-int speedInterrupts = 0;
-unsigned long firstSpeedInterruptTime = 0;
+bool isVerbose = true;
 
-bool skippedLastInputEvent = false;
+// // Transmission speed variables
+// int transmissionSpeed = 0;
+// int speedInterrupts = 0;
+// unsigned long firstSpeedInterruptTime = 0;
 
-bool inputCurrentStateHigh = false;
-int lastChangeTime = 0;
-int inputClockPeriod = 500;
-int outputClockPeriod = 500;
+// bool skippedLastInputEvent = false;
 
-system_tick_t lastThreadTime = 0;
-system_tick_t lastMessageTime = 0;
+// bool inputCurrentStateHigh = false;
+// int lastChangeTime = 0;
+// int inputClockPeriod = 500;
+// int outputClockPeriod = 500;
 
-int bitCounter = 0;
+// system_tick_t lastThreadTime = 0;
+// system_tick_t lastMessageTime = 0;
+
+// int bitCounter = 0;
 
 void setup() {
 	Serial.begin(9600);
 
-    pinMode(outputPin, OUTPUT_OPEN_DRAIN);
-    pinMode(inputPin, INPUT_PULLUP);
-
-    digitalWrite(outputPin, LOW);
-
-    attachInterrupt(inputPin, inputEvent, CHANGE);
-    CurrentInputState = initial;
-
-    msgManager = MessageManager();
+    BitManagerSetup();
 
     waitFor(Serial.isConnected, 30000);
     Serial.println("Serial connected: starting");
-    Thread thread("outputThread", outputThread);
+    //Thread thread("outputThread", outputThread);
 }
 
 void loop() {
+
+    delay(5000);
+    uint8_t message1[4] = {0b01010101, 0b10101010, 0b00001111, 0b11110000};
+    uint8_t messageSize = 4;
+    sendMessage(message1, messageSize);
+
+
 	// Do we really need to make a thread if we are not using main thread (do interrupts run on main thread??)
 
     // Call messages to send here, split by long delays?
     // char* message1 = [H, e, l, l, o, , W, o, r, l, d, !];
     // ex. msgManager.sendMessage(message1);
     // delay(10000); OR os_thread_delay_until(&lastMessageTime, 10000)
-}
-
-
-// ----------
-// HANDLE INPUT 
-// ----------
-
-
-bool getTransmissionSpeed(){
-    if (speedInterrupts < 7){
-        if (speedInterrupts == 0)
-            firstSpeedInterruptTime = millis();
-        speedInterrupts++;
-        return false;
-    }
-    else{
-        // Get time since last interrupt
-        unsigned long currentTime = millis();
-        int elapsedTime = currentTime - firstSpeedInterruptTime;
-        
-        // Calculate speed by meaning
-        transmissionSpeed = elapsedTime/7/2;
-
-        speedInterrupts = 0; // Reset counter
-
-        Serial.printlnf("Clock speed: %d", transmissionSpeed);
-        inputClockPeriod = transmissionSpeed; // Set global clock speed variable
-
-        return true;
-    }
-};
-
-// Function used to change state and perform necessary actions right away (like outputing)
-void changeInputState(InputState newInputState) {
-    switch (newInputState) {
-        case output0:
-            // Register that a 0 has been read
-            Serial.println("READ: 0");
-            msgManager.frameManager.receiveBit(0b0);
-            break;
-        case output1:
-            // Register that a 1 has been read
-            Serial.println("READ: 1");
-            msgManager.frameManager.receiveBit(0b1);
-            break;
-    }
-    // Serial.printlnf("Received bit number %d", bitCounter++);
-    CurrentInputState = newInputState; // Change to new state for next event
-}
-
-void inputEvent() {
-
-    // Compute transmission speed at the beginning of each frame
-    // if (msgManager.frameManager.currentReceivingState == preambule) {
-    //     bool speedComputeComplete = getTransmissionSpeed();
-    //     if (speedComputeComplete) {
-    //         // Done receiving preambule bits
-    //         msgManager.frameManager.receiveData(0b01010101); // Notify msgManager that preambule has been received
-    //     }
-    //     return;
-    // }
-    //----------
-
-    int duration = millis() - lastChangeTime;
-    lastChangeTime = millis();
-
-    // If 80% higher than one clock period: must be two periods (AKA: long period)
-    int longPeriodMin = inputClockPeriod * 1.8;
-    int longPeriodMax = inputClockPeriod * 2.2;
-    int shortPeriodMin = inputClockPeriod * 0.8;
-
-    if(duration < shortPeriodMin) {
-        //Serial.printlnf("Rejecting too short impulse of %d ms", duration);
-        return;
-    }
-
-    // Determine newStateDuration (time since last change event)
-    StateDuration newStateDuration;
-    if (duration > longPeriodMax) {
-        newStateDuration = veryLongPeriod;
-    }
-    else if (duration >= longPeriodMin && duration < longPeriodMax) {
-        newStateDuration = longPeriod;
-    }
-    else if (duration >= shortPeriod && duration < longPeriodMin) {
-        newStateDuration = shortPeriod;
-    }
-    
-    
-    // Printing (debug)
-    //Serial.printlnf("Read %s impulse duration: %d ms -> #%d (CurrentInputState: %d)", inputCurrentStateHigh ? "HIGH" : "LOW", duration, newStateDuration, CurrentInputState);
-    inputCurrentStateHigh = !inputCurrentStateHigh;
-
-    // STATE MACHINE: Decode Manchester
-    switch (CurrentInputState) {
-        case initial:
-            if (newStateDuration == shortPeriod || newStateDuration == veryLongPeriod) {
-                changeInputState(output0);
-            } 
-            else {
-                Serial.println("ERROR: initial state got longPeriod");
-            }
-            break;
-
-        case output0:
-            if (newStateDuration == shortPeriod) {
-               changeInputState(waitShort);
-            }
-            else if (newStateDuration == longPeriod) {
-               changeInputState(output1);
-            }
-            // veryLongPeriod -> stay in output0
-            break;
-
-        case waitShort:
-            if (newStateDuration != shortPeriod) {
-                Serial.printlnf("ERROR: expected shortPeriod in wait state got #%d", newStateDuration);
-            }
-            changeInputState(output0);
-            break;
-        
-        case output1:
-            if (newStateDuration == shortPeriod) {
-                changeInputState(waitLong);
-            }
-            else if (newStateDuration == longPeriod) {
-                changeInputState(output0);
-            }
-            else {
-                Serial.println("UNDEFINED behaviour for veryLongPeriod in inputState 'output1'");
-            }
-
-            break;
-        
-        case waitLong:
-            if (newStateDuration != shortPeriod) {
-                Serial.printlnf("ERROR: expected shortPeriod in wait state got #%d", newStateDuration);
-            }
-            changeInputState(output1);
-            break;
-    }
-}
-
-
-// ----------
-// HANDLE OUTPUT 
-// ----------
-
-
-void output(PinState level) {
-    digitalWrite(outputPin, level);
-    os_thread_delay_until(&lastThreadTime, outputClockPeriod);
-}
-
-void sendBitsManchester(bool bits[], int bitCount) {
-    for (int i = 0; i < bitCount; i++) {
-        if (bits[i] == BIT1) {
-            // Send 1 in Manchester
-            //Serial.println("SEND: 1");
-            output(HIGH);
-            output(LOW);
-        }
-        else {
-            // Send 0 in Manchester
-            //Serial.println("SEND: 0");
-            output(LOW);
-            output(HIGH);
-        }
-    }
-}
-
-void outputThread() {
-    //delay(5000);
-    Serial.println("Starting output loop");
-    while(true) {
-        
-        bool bitsToSend[] = {BIT0, BIT1, BIT1, BIT1, BIT0, BIT1, BIT0, BIT1};
-        sendBitsManchester(bitsToSend, 8);
-        Serial.println("---------");
-        os_thread_delay_until(&lastThreadTime, 2000);
-        CurrentInputState = initial;
-	}
 }
 
